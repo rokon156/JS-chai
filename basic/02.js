cc593022aef443caf1e31ca47fd35f7f3f7f6063
rokon156 <81416087+rokon156@users.noreply.github.com>
2024-10-13 05:07:45 +0000

    convertion of data types

---
+++ basic/02.js
@@ -0,0 +1,16 @@
+let score = undefined;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+//NaN-->"Not a Number" [Special Type]
+// "33" => 33
+// "33qbc" => NaN
+// true => 1
+// false => 0
+// "ROKON" => NaN
+// "undefiend" => 
