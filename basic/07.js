3aea24ff224b51afcfa84a0cf9b97a1dfb7dbe55
rokon156 <81416087+rokon156@users.noreply.github.com>
2024-10-14 00:26:12 +0000

    Day-2

---
+++ basic/07.js
@@ -0,0 +1,35 @@
+let value = 3;
+let newValue = -value;
+
+//console.log(newValue);
+
+let str1 = "ROKON";
+let str2 = " ZZAMAN";
+let str3 = str1+str2;
+
+// console.log(str3);
+// console.log(str1+str2);
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+// console.log("");
+// console.log(+"");
+
+let n1 = n2 = n3 = 5;
+//console.log(n2);
+
+let n = 5;
+console.log(n++);
+console.log(n);
+console.log(++n);
+console.log(--n);
+
+
+console.log();
+
